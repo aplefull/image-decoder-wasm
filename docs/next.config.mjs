3d38a32d12157e48f3d698000deb7d44a83a1d42
aplefull <aplefull@gmail.com>
2025-10-20 14:54:40 +0200

    Setup GitHub Pages and prepare for npm publish

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -2,8 +2,11 @@ import nextra from 'nextra';
 
 const withNextra = nextra({});
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default withNextra({
   output: 'export',
+  basePath: isProd ? '/image-decoder-wasm' : '',
   images: {
     unoptimized: true,
   },
